87d071782fa20fad20ded38508b855cca961596e
Riz-baig <baigrizmirza@gmial.com>
2026-04-24 18:53:16 +0200

    debuguear nuevamente

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     
     public function login(Request $request)
     {
-        dd($request->all());
+        
         $user = DB::table('users')
             ->where('email', $request->email)
             ->first();
