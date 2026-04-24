5ceb54c920c2db052664a5c3db40507b27d1bd95
Riz-baig <baigrizmirza@gmial.com>
2026-04-24 15:26:43 +0200

    modificado nuevamente la ruta css

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistema de Triaje</title>
 
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <link rel="stylesheet" href="{{ asset('/css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('public/css/login.css') }}">
     
 </head>
 <body>
